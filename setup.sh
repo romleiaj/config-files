33bcc5c91742f08f74fff0ce93663f7bb38b1663
Adam Romlein <35015390+romleiaj@users.noreply.github.com>
2019-10-27 09:39:43 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,13 +2,20 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# Install some basic packages guaranteed to be used
 sudo apt-get install \
 htop \
 vim \
 tmux
 
+# Link dotfiles
 ln -s ${DIR}/.tmux.conf ~/.tmux.conf
 ln -s ${DIR}/.vimrc ~/.vimrc
 
+# Install vim theme solarized
 source ${DIR}/install_pathogen.sh
 source ${DIR}/install_solarized.sh
+
+# setup default git
+git config --global user.email "romleiaj@clarkson.edu"
+git config --global user.name  "Adam Romlein"
